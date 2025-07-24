b53fb04186155b16cdd40bdc8f027e0d183cadba
Abdul Azeem <abdulazeem@devnexus.in>
2025-07-24 15:32:54 +0530

    Add webhook lead gen

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,5 +1,6 @@
 // routes/webhook.js
 import MetaLeadsCollection from "../models/MetaLeadsModel.js";
+import TokenModel from "../models/Token.js";
 
 const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 
@@ -28,7 +29,7 @@ export const handleFacebookWebhook = async (req, res) => {
         const leadgen_id = change.value.leadgen_id;
         const form_id = change.value.form_id;
 
-        const tokenData = await Token.findOne({ page_id: pageId });
+        const tokenData = await TokenModel.findOne({ page_id: pageId });
         if (!tokenData) return;
 
         const leadRes = await axios.get(`https://graph.facebook.com/v19.0/${leadgen_id}?access_token=${tokenData.page_access_token}`);
--- models/MetaLeadsModel.js
+++ models/MetaLeadsModel.js
@@ -1,31 +1,36 @@
 import mongoose from "mongoose";
 
 const leadSchema = new mongoose.Schema({
-  leadgen_id: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  form_id: {
-    type: String,
-    required: true,
-  },
-  page_id: {
-    type: String,
-    required: true,
-  },
-  field_data: [
-    {
-      name: String,
-      values: [String],
+    //   leadgen_id: {
+    //     type: String,
+    //     required: true,
+    //     unique: true,
+    //   },
+    //   form_id: {
+    //     type: String,
+    //     required: true,
+    //   },
+    //   page_id: {
+    //     type: String,
+    //     required: true,
+    //   },
+    //   field_data: [
+    //     {
+    //       name: String,
+    //       values: [String],
+    //     },
+    //   ],
+    leadgen_id: String,
+    form_id: String,
+    page_id: String,
+    field_data: Array,
+    created_time: String,
+    created_time: {
+        type: Date,
+        required: true,
     },
-  ],
-  created_time: {
-    type: Date,
-    required: true,
-  },
 }, {
-  timestamps: true, // adds createdAt and updatedAt fields automatically
+    timestamps: true, // adds createdAt and updatedAt fields automatically
 });
 
 const MetaLeadsCollection = mongoose.model("MetaLeadsCollection", leadSchema);
